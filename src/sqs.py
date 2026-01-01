36dc4cf1ad9cb2b7ca33fd1834afac74cfc5a50e
wadhwarochak <rochak.wadhwa@gmail.com>
2026-01-01 10:02:59 +0530

    Updated recent changes

--- src/sqs.py
+++ src/sqs.py
@@ -1,6 +1,10 @@
 import boto3
 
 QUEUE_NAME = 'MyTest-SQS-Queue'
+QUEUE_NAME_URL = "https://sqs.ap-south-1.amazonaws.com/743472496366/MyTest-SQS-Queue"
+FIFO_QUEUE_NAME = 'MyTestQueue.fifo'
+
+
 def sqs_client():
     sqs = boto3.client('sqs', region_name='ap-south-1')
     return sqs
@@ -13,6 +17,14 @@ def create_sqs_queue():
     )
 
 
+def create_fifo_queue():
+    return sqs_client().create_queue(
+        QueueName=FIFO_QUEUE_NAME,
+        Attributes={
+            "FifoQueue": 'true'
+        }
+    )
+
 if __name__ == '__main__':
-    print(create_sqs_queue())
-    
+    #print(create_sqs_queue())
+    print(create_fifo_queue())
